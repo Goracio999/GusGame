a073f7676964b7817e76bb82104ff85ec9b29725
Goracio999 <gora999@gmail.com>
2023-07-10 18:01:31 +0300

    Floating Square

--- main.py
+++ main.py
@@ -6,19 +6,47 @@ pygame.init()
 print("Hello World! Let's go!")
 
 HEIGHT = 800
-WIDTH = 1200
+WIDTH = 600
+
+COLOR_WHITE = (255, 255, 255)
+COLOR_BLACK = (0, 0, 0)
+
+FPS = pygame.time.Clock()
 
 main_display = pygame.display.set_mode((HEIGHT, WIDTH))
 
-player = pygame.Surface((20, 20))
-player.fill((255, 255, 255))
+player_size = (20, 20)
+
+player = pygame.Surface(player_size)
+player.fill(COLOR_WHITE)
+player_rect = player.get_rect()
+player_speed = [1, 1]
+
 playing = True
+
 while playing: 
-    print("Hello from pygame")
+    # print("Hello from pygame")
+    FPS.tick(120)
     for event in pygame.event.get():
         if event.type == QUIT:
             playing = False
-    
-    main_display.blit(player, (0, 0))
+
+    main_display.fill(COLOR_BLACK)
+
+    if player_rect.bottom >= HEIGHT:
+        player_speed[1] = -player_speed[1]
+
+    if player_rect.top < 0:
+        player_speed[1] = -player_speed[1]    
+
+    if player_rect.right >= WIDTH:
+        player_speed[0] = -player_speed[0]
+
+    if player_rect.left < 0:
+        player_speed[0] = -player_speed[0]    
+
+    main_display.blit(player, player_rect)
+    print("bottom:",player_rect.bottom," right:",player_rect.right)
+    player_rect = player_rect.move(player_speed)
 
     pygame.display.flip()
