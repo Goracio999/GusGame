93f0636daad514285378319a5e563ffb302a3137
Goracio999 <gora999@gmail.com>
2023-07-12 16:52:56 +0300

    Final game
    With animated player and images for enemies, bonuses and scrolled background

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import random
 import pygame
 from pygame.constants import QUIT, K_DOWN, K_UP, K_LEFT, K_RIGHT
@@ -6,8 +7,8 @@ pygame.init()
 
 print("Hello World! Let's go!")
 
-HEIGHT = 800
-WIDTH = 600
+HEIGHT = 1000
+WIDTH = 1600
 
 FONT = pygame.font.SysFont('Verdana', 20)
 
@@ -19,49 +20,71 @@ COLOR_BLUE = (0, 0, 255)
 COLOR_GREEN = (0, 255, 0)
 COLOR_YELLOW = (255, 255, 0)
 
+# adding backgroun image
+back_ground = pygame.transform.scale(pygame.image.load('imgs/background.png'), (WIDTH, HEIGHT))
+back_ground_X1 = 0
+back_ground_X2 = back_ground.get_width()
+back_ground_move = 2
+
 FPS = pygame.time.Clock()
 
 main_display = pygame.display.set_mode((WIDTH, HEIGHT))
 display_color = COLOR_BLUE
 
 # Initialize player
-player_size = (20, 20)
-player = pygame.Surface(player_size)
+IMAGE_PATH = "imgs/animation"
+PLAYER_IMAGES = os.listdir(IMAGE_PATH)
+print(PLAYER_IMAGES)
+# player = pygame.Surface(player_size)
+player = pygame.image.load('imgs/player.png').convert_alpha()
+player_width = player.get_width()
+player_height = player.get_height()
+print ("Player size is: ", player_width, " x ", player_height)
+player_size = (player_width, player_height)
 player_color = COLOR_YELLOW
-player.fill(player_color)
+# player.fill(player_color)
 player_rect = player.get_rect()
-player_move_down = [0, 1]
-player_move_up = [0, -1]
-player_move_left = [-1, 0]
-player_move_right = [1, 0]
+player_move_down = [0, 4]
+player_move_up = [0, -4]
+player_move_left = [-4, 0]
+player_move_right = [4, 0]
 player_score = 0
+# initialize animation
+CHANGE_IMAGE = pygame.USEREVENT
+pygame.time.set_timer(CHANGE_IMAGE, 150)
+image_index = 0
 
 # Initialize enemy
 def create_enimy():
-    enemy_size = (30, 30)
-    enemy_color = COLOR_RED
-    enemy = pygame.Surface(enemy_size)
-    enemy.fill(enemy_color)
-    enemy_rect = pygame.Rect(WIDTH, random.randint(0, HEIGHT), *enemy_size)
+    # enemy_color = COLOR_RED
+    # enemy = pygame.Surface(enemy_size)
+    # enemy.fill(enemy_color)
+    enemy = pygame.image.load('imgs/enemy.png').convert_alpha()
+    enemy_size = (enemy.get_width(), enemy.get_height())
+    print("Enemy size is: ", enemy_size)
+    enemy_rect = pygame.Rect(WIDTH, random.randint(0, (HEIGHT-enemy.get_height())), *enemy_size)
     enemy_speed = [random.randint(-5, -1), 0]
     return [enemy, enemy_rect, enemy_speed]
 
 CREATE_ENEMY = pygame.USEREVENT + 1
-pygame.time.set_timer(CREATE_ENEMY, 1500)
+pygame.time.set_timer(CREATE_ENEMY, 2500)
 enemies = []
 
 # Initialize bonus
 def create_bonus():
-    bonus_size = (20, 20)
-    bonus_color = COLOR_GREEN
-    bonus = pygame.Surface(bonus_size)
-    bonus.fill(bonus_color)
-    bonus_rect = pygame.Rect(random.randint(0, WIDTH), 0, *bonus_size)
+    # bonus_size = (20, 20)
+    # bonus_color = COLOR_GREEN
+    # bonus = pygame.Surface(bonus_size)
+    # bonus.fill(bonus_color)
+    bonus = pygame.image.load('imgs/bonus.png').convert_alpha()
+    bonus_size = (bonus.get_width(), bonus.get_height())
+    print("Bonus size is: ", bonus_size)
+    bonus_rect = pygame.Rect(random.randint(0, (WIDTH-bonus.get_width())), 0, *bonus_size)
     bonus_move = [0, 1]
     return [bonus, bonus_rect, bonus_move]
 
 CREATE_BONUS = pygame.USEREVENT + 2
-pygame.time.set_timer(CREATE_BONUS, 2500)
+pygame.time.set_timer(CREATE_BONUS, 3000)
 bonuses = []
 
 # Starting game process
@@ -77,9 +100,24 @@ while playing:
             enemies.append(create_enimy())
         if event.type == CREATE_BONUS:
             bonuses.append(create_bonus())
+        if event.type == CHANGE_IMAGE:
+            player = pygame.image.load(os.path.join(IMAGE_PATH, PLAYER_IMAGES[image_index]))
+            image_index += 1
+            if image_index >= len(PLAYER_IMAGES):
+                image_index = 0
 
-
-    main_display.fill(display_color)
+    # animate backgroud
+    if back_ground_X1 < -back_ground.get_width():
+        back_ground_X1 = back_ground.get_width()
+    else: 
+        back_ground_X1 -= back_ground_move
+    if back_ground_X2 < -back_ground.get_width():
+        back_ground_X2 = back_ground.get_width()
+    else: 
+        back_ground_X2 -= back_ground_move  
+    
+    main_display.blit(back_ground, (back_ground_X1, 0))
+    main_display.blit(back_ground, (back_ground_X2, 0))
 
 # Arrow keys processing - drive the player
     keys = pygame.key.get_pressed()
@@ -118,7 +156,7 @@ while playing:
         print("left", player_rect.left,"top:",player_rect.top,"bottom:",player_rect.bottom," right:",player_rect.right)
 
     main_display.blit(player, player_rect)
-    main_display.blit(FONT.render(str(player_score), True, COLOR_WHITE), (WIDTH-50, 20))
+    main_display.blit(FONT.render(("SCORE: " + str(player_score)), True, COLOR_GREEN), (WIDTH-200, 20))
 
 # processing enemies
     for enemy in enemies:
